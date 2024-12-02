ffb2d2f9476cb42b931bcec5569e34f03a4a7a17
Hossein Ramezani <wave1221ram@gmail.com>
2024-12-02 15:33:02 +0330

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import global.*;
+import person.*;
+import school.*;
+
+class Second {
+    public static void main(String[] args) {
+        String a = "test";
+        System.out.println("test");
+        EducationGrade obj = new EducationGrade();
+
+    }
+}
